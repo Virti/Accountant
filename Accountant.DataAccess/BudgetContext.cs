1498db917ad2f68e53cbcbca91eac372496b0bb3
Krystian Sitarek <krystiansitarek@gmail.com>
2017-11-18 19:29:01 +0100

    Added Accountant.DataAccess responsible for allowing and restricting access to a cetrain data stores.

---
+++ Accountant.DataAccess/BudgetContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Accountant.Domain.Budget;
+using Microsoft.EntityFrameworkCore;
+
+namespace Accountant.DataAccess
+{
+    public class BudgetContext : DbContext {
+        public DbSet<MonthBudget> MonthBudgets { get; set; }
+        public DbSet<Operation> Operations { get; set; }
+        public DbSet<OperationCategory> OperationCategories { get; set; }
+    }
+}
